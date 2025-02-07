0d30699a8b303e5d4d5153cb393b7b7688b06b90
shubhswain45 <shubhkantswain@.com>
2025-02-07 09:51:14 +0530

    jjjj

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,23 +1,26 @@
-import { json, type LoaderFunction } from "@remix-run/node";
+import type { LoaderFunction } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 
-// Simulated delay function
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-// Server-side loader function (Waits for 20s before returning response)
-export const loader: LoaderFunction = async () => {
-  await delay(5000); // Wait for 20 seconds
+
+export const loader: LoaderFunction = async ({ context, params }) => {
+  const { env, cf, ctx } = context.cloudflare;
+   await delay(5000); // Wait for 20 seconds
 
   return json({ message: "Hello after 20 seconds!" });
 };
 
 export default function Index() {
-  const data = useLoaderData<typeof loader>();
-
+  const results = useLoaderData<typeof loader>();
   return (
-    <div className="text-white text-center mt-10">
-      <h1 className="text-2xl font-bold">Server-Side Rendered Page</h1>
-      <p className="mt-2 text-lg">{data.message}</p>
+    <div>
+      <h1>Welcome to Remix</h1>
+      <div>
+        A value from D1:
+        <pre>{JSON.stringify(results)}</pre>
+      </div>
     </div>
   );
 }
